1f4c61c17333e095447bb3cc42e4594f11f7469b
Oleg <gordienko21061988@gmail.com>
2023-10-10 14:54:39 +0300

    added tests for TextBox

--- src/test/java/guru/qa/TextBoxTests.java
+++ src/test/java/guru/qa/TextBoxTests.java
@@ -1,14 +1,44 @@
 package guru.qa;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class TextBoxTests {
 
+    @BeforeAll
+    static void setUp() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.timeout = 15000;
+        Configuration.browserSize = "1366x768";
+
+
+    }
+
     @Test
     void fillFormTests() {
-     open("https://demoqa.com/text-box");
+     open("/text-box");
+
+     String name = "Oleg Petrov";
+
+     $("[id=userName]").setValue("name");
+     $("[id=userEmail]").setValue("oleg@petrov.com");
+     $("[id=currentAddress]").setValue("Some street 1");
+     $("[id=permanentAddress]").setValue("Another street 2");
+     $("[id=submit]").click();
+
+     $("[id=output]").shouldHave(text("name"), text("oleg@petrov.com"),
+             text("Some street 1"), text("Another street 2"));
+
+        $("[id=output] [id=name]").shouldHave(text("name"));
+        $("[id=output]").$("[id=name]").shouldHave(text("name"));
+
+        $("[id=permanentAddress]").shouldHave(text("name"));
 
     }
 }
