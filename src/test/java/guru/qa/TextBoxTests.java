b31bbf8b410d1ea95f02dc631e0350df736c7346
Oleg <gordienko21061988@gmail.com>
2023-10-10 10:05:30 +0300

    small refactor & added logger slf4j

---
+++ src/test/java/guru/qa/TextBoxTests.java
@@ -0,0 +1,15 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTests {
+
+    @Test
+    void fillFormTests() {
+     open("https://demoqa.com/text-box");
+
+    }
+}
+
