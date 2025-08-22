ef4a044b991f3f24c3cfe7176541a0a59e4f371a
gordi <gordienkoos@krlb.ru>
2025-08-22 10:18:35 +0300

    Написание новых тестов

--- src/test/java/guru/qa/TextSetup.java
+++ src/test/java/guru/qa/TextSetup.java
@@ -1,6 +1,10 @@
 package guru.qa;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -9,10 +13,12 @@ import java.util.Map;
 public class TextSetup {
     @BeforeAll
     static void beforeAll() {
-        Configuration.holdBrowserOpen = false;
+        Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
-     // Configuration.browser = "firefox";
-        Configuration.pageLoadStrategy = "eager";Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.timeout = 10000;
+        //Configuration.holdBrowserOpen = true;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String,Object>of(
                 "enableVNC", true,
@@ -20,5 +26,16 @@ public class TextSetup {
         ));
         Configuration.browserCapabilities = capabilities;
 
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
+
+
