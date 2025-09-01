12c29ef9f876748262466bb54b26015421f799a5
gordi <gordienkoos@krlb.ru>
2025-09-01 07:07:17 +0300

    Написание новых тестов

--- src/test/java/guru/qa/TextSetup.java
+++ src/test/java/guru/qa/TextSetup.java
@@ -17,7 +17,6 @@ public class TextSetup {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
         Configuration.timeout = 10000;
-        //Configuration.holdBrowserOpen = true;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String,Object>of(
