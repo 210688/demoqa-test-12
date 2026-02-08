076de7e0d00a7fdb4ee4f2bb5a92f341db0c5dbc
gordi <gordienkoos@krlb.ru>
2026-02-08 21:38:35 +0300

    Написание новых тестов

--- src/test/java/practice/form/TestPracticeForm.java
+++ src/test/java/practice/form/TestPracticeForm.java
@@ -12,7 +12,6 @@ public class TestPracticeForm extends TextSetup {
     RegistrationFormPage registrationFormPage;
     TestDataGenerator testDataGenerator;
 
-
     @BeforeEach
     void init() {
         registrationFormPage = new RegistrationFormPage(new Faker());
